ddff2c5099634dbb360abea22046385a3ed379dc
LimaAlysson <alyssonredes32@gmail.com>
2025-03-07 10:28:02 -0300

    feature/adicionando tasks e tasks assíncronas

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,17 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+    })
+
+    grunt.registerTask('olaMundo', function() {
+        const done = this.async();
+        setTimeout(function() {
+            grunt.log.write('Olá mundo');
+            done();
+        }, 10000);
+    });
+
+    grunt.registerTask('default', ['olaMundo']);
+
+};
+
